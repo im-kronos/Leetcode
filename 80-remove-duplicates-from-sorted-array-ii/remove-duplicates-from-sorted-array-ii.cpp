9b26a672fe7e63bda19c106548792f0ca092ee3c
Dharshan <66472781+im-kronos@users.noreply.github.com>
2023-12-23 02:45:40 +0530

    Time: 4 ms (67.66%) | Memory: 11.4 MB (30.47%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-     unordered_map<int,int>map;
-     int size=nums.size(); int counter=0;
-     for(int i=0;i<size;i++)
-     {
-         int a=nums[i];
-         if(map.find(a)==map.end())
-         {
-             map[a]=0;
-         }
-         if(map.find(a)!=map.end() && map[a]<2)
-         {
-             nums[counter]=a;
-             counter++;
-             map[a]++;
-         }
-     }
-
-     return counter;
-        
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(count<2 || nums[count-2]!=nums[i]){
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
     }
 };
