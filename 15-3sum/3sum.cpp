47766196a07c6b0f13685c0c63e479fc052189e8
Dharshan <66472781+im-kronos@users.noreply.github.com>
2023-12-25 17:52:10 +0530

    Time: 102 ms (82.20%) | Memory: 26.4 MB (48.09%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,36 @@
 class Solution {
 public:
+    // Function to find all unique triplets in the array that sum up to zero
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
+        vector<vector<int>> ans; // Vector to store the triplets
+
+        // Sorting the input array to simplify the solution
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size() - 1; i++) {
-            // Initialize first element nums[i]
+            // Initialize the first element of the triplet
             if (i > 0 && nums[i] == nums[i - 1])
-                continue;
+                continue; // Skip duplicate elements to avoid duplicate triplets
 
-            int j = i + 1;
-            int k = nums.size() - 1;
+            int j = i + 1; // Pointer for the second element of the triplet
+            int k = nums.size() - 1; // Pointer for the third element of the triplet
 
             while (j < k) {
-                int currentSum = nums[i] + nums[j] + nums[k];
+                int currentSum = nums[i] + nums[j] + nums[k]; // Calculate the sum of the triplet
 
                 if (currentSum < 0) {
-                    j++;
+                    j++; // Move the second pointer to the right to increase the sum
                 } else if (currentSum > 0) {
-                    k--;
+                    k--; // Move the third pointer to the left to decrease the sum
                 } else {
-                    vector<int> temp = {nums[i], nums[j], nums[k]};
-                    ans.push_back(temp);
-                    j++;
-                    k--;
+                    // Found a triplet with sum zero
+                    vector<int> temp = {nums[i], nums[j], nums[k]}; // Create a triplet vector
+                    ans.push_back(temp); // Add the triplet to the result vector
 
-                    // Skip duplicate elements
+                    j++; // Move the second pointer to the right
+                    k--; // Move the third pointer to the left
+
+                    // Skip duplicate elements to avoid duplicate triplets
                     while (j < k && nums[j] == nums[j - 1])
                         j++;
                     while (j < k && nums[k] == nums[k + 1])
@@ -33,6 +38,7 @@ public:
                 }
             }
         }
-        return ans;
+
+        return ans; // Return the vector containing all unique triplets
     }
 };
