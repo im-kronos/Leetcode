d5e77d15c15963cd003a6ef0d17bf74d38864e88
Dharshan <66472781+im-kronos@users.noreply.github.com>
2024-01-19 23:06:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (9.01%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -4,7 +4,7 @@ public:
         int cnt=0;
         while(n)
         {
-            n=n&(n-1);   //update n where there is rightmost 1 {using n&(n-1)}
+            n=n&(n-1);   //update last 1 to 0 each time where there is rightmost 1 {using n&(n-1)}
             cnt++;
         }
         return cnt;
