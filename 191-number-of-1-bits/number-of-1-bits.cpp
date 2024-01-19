5cf0afcab240bdd174155920a38e7d3e988ec7f0
Dharshan <66472781+im-kronos@users.noreply.github.com>
2024-01-19 23:00:41 +0530

    Time: 4 ms (32.28%) | Memory: 7 MB (9.01%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt=0;
+        while(n)
+        {
+            n=n&(n-1);   //update n where there is rightmost 1 {using n&(n-1)}
+            cnt++;
+            cout<<n<<" ";
+        }
+        return cnt;
+    }
+};
