66378d953afefa194f1c4afc4279764fb84a755f
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-03-05 10:30:11 +0530

    Time: 6 ms (40.46%) | Memory: 13.1 MB (56.36%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.cpp
+++ 985-bag-of-tokens/bag-of-tokens.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& tokens, int power) {
-        sort(tokens.begin(), tokens.end());               // sort the token 
-        int s = 0;                                       // Initialize the Score
-        int maxi = 0;                                   // Track the maximum score
-        int l = 0, r = tokens.size() - 1;              // Left and Right pointer 
-        while (l <= r) { 
-            if (power >= tokens[l]) {                // If the remaining power is greater than or equal to the smallest token
-                power -= tokens[l];                 // Reduce the Power 
-                s++;                               // Increase the score
-                l++;                              // Move the left pointer to the right.
-                maxi = max(maxi, s);             // Update the maximum score if the current score is higher.score
-            } 
-            else if (s > 0) {                  // If the current score is positive, consider trading tokens for power.
-                power += tokens[r];           // increase the power
-                s--;                         // Decrease the score.
-                r--;                        // Move the right pointer to the left.
-            } 
-            else {
-                break;                  // exit the loop if neither condition is met
+        sort(tokens.begin(),tokens.end());
+        int s=0;
+        int l=0;
+        int r=tokens.size()-1;
+        int maxi=0;
+        while(l<=r)
+        {
+            if(power>=tokens[l])
+            {
+                power-=tokens[l];
+                l++;
+                s++;
+                maxi=max(s,maxi);
             }
-        }
+            else if(s>0)
+            {
+                power+=tokens[r];
+                s--;
+                r--;
+            }
+            else
+            {
+                break;
+            }
 
+        }
         return maxi;
     }
 };
