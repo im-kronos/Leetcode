9e014527d2d4593e8dabbff547e7b0bae512a8d7
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-03-27 02:07:19 +0530

    Time: 47 ms (43.80%) | Memory: 43.4 MB (98.86%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,24 +1,20 @@
-#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n=nums.size();
-        nums.push_back(INT_MIN);//make index easier handling
-        for(int i=0; i<=n; i++){
-            while( 0<nums[i] && nums[i]<=n // x in [1, n]
-            && nums[i]!=nums[nums[i]]) //not in the right place  
-                swap(nums[i], nums[nums[i]]); //sort by swap
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
-        for (int i=1; i<=n; i++)
-            if (nums[i]!=i) return i;
-        return n+1;
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        
+        return n + 1;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
