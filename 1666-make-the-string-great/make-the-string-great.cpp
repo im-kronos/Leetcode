ca7e27576e92c248e6659858c7700b39672cd7af
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-04-06 00:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (16.50%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    string makeGood(std::string s) {
-        stack<char> stack;
-        
-        for (char c : s) {
-            if (!stack.empty() && abs(c - stack.top()) == 32) {
-                stack.pop();
-            } else {
-                stack.push(c);
+    string makeGood(string s) {
+        stack<char> st;
+        for(auto ch : s){
+            if(st.empty())   st.push(ch);
+            else{
+                if(st.top()+32 == ch || st.top()-32 == ch)
+                    st.pop();
+                else
+                    st.push(ch);
             }
         }
-        
-        string result;
-        while (!stack.empty()) {
-            result = stack.top() + result;
-            stack.pop();
+        string res;
+        while(!st.empty()){
+            res = st.top()+res;
+            st.pop(); 
         }
-        
-        return result;
+        return res;
     }
 };
