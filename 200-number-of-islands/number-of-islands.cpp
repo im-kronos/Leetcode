e27db191fa854301c0f302854480e1d8eafb329a
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-04-19 21:00:26 +0530

    Time: 27 ms (71.43%) | Memory: 15.9 MB (67.22%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+
+    int check(vector<vector<char>>&grid,int &count,int i,int j)
+    {
+        //base exit condition
+        if (i >= grid.size() || j >= grid[0].size() || grid[i][j] != '1') {
+            return 0;
+        }
+
+        // Mark current cell as visited
+        grid[i][j] = '0';
+
+        //trverse
+        check(grid,count,i+1,j);
+        check(grid,count,i,j+1);
+        check(grid,count,i-1,j);
+        check(grid,count,i,j-1);
+        return 1;
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int count=0;
+        for(int i=0;i<grid.size();i++)
+        {
+            for(int j=0;j<grid[0].size();j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count+=check(grid,count,i,j);
+                }
+
+            }
+        }
+        
+        return count;
+ 
+    }
+};
