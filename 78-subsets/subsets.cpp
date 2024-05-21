2adc86c8356e9e7ec700c534d8c20b3e31dee4de
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-05-21 23:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (40.42%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-    int n = nums.size();
-    int totalSubsets = 1 << n; // Total number of subsets is 2^n
-    vector<vector<int>> ans;
-
-    for (int i = 0; i < totalSubsets; ++i) {
-        vector<int> currSubset;
-        for (int j = 0; j < n; ++j) {
-            if (i & (1 << j)) {
-                currSubset.push_back(nums[j]);
-            }
+    void subset_maker(vector<int>& nums, int i, vector<int>& current, vector<vector<int>>& ans) {
+        if (i == nums.size()) {
+            ans.push_back(current);
+            return;
         }
-        ans.push_back(currSubset);
+        
+
+        subset_maker(nums, i + 1, current, ans);
+
+        current.push_back(nums[i]);
+        subset_maker(nums, i + 1, current, ans);
+        current.pop_back(); 
     }
 
-    return ans;
-}
-
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> current;
+        subset_maker(nums, 0, current, ans);
+        return ans;
+    }
 };
