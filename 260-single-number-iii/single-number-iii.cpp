a0f48b1bb7d6102c945ef09c2ddb46ac1b58beb1
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-06-01 01:02:37 +0530

    Time: 10 ms (38.25%) | Memory: 12.3 MB (64.77%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int getBit(int x, int k) {
-        return (x >> k) & 1;
-    }
-
     vector<int> singleNumber(vector<int>& nums) {
-        int xorXY = 0;
-        for (int num : nums)
-            xorXY ^= num;
-
-        int pos = 0;
-        while (!getBit(xorXY, pos)) pos++;
-
-        int x = 0;
-        for (int num : nums)
-            if (getBit(num, pos))
-                x ^= num;
-
-        int y = xorXY ^ x;
-        return {x, y};
+        long temp=0;
+        for(int i=0;i<nums.size();i++){
+            temp^=nums[i];
+        } 
+        int res=(temp & temp-1)^temp;
+        int first=0;
+        int second=0;
+        for(int i=0;i<nums.size();i++){
+            if(res&nums[i]){
+                first^=nums[i];
+            }else{
+                second^=nums[i];
+            }
+        }
+        return {first,second};
     }
 };
