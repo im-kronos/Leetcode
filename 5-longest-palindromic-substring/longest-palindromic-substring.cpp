41ec799fef7029056d8fcadcbddd4ce110182914
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-06-05 05:03:51 +0530

    Time: 8 ms (91.90%) | Memory: 7.9 MB (99.92%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
-     string longestPalindrome(string s) 
-{   
-    int len = s.size();
-    int dp[len][len];
-    memset(dp,0,sizeof(dp));
-    int end=1;
-    int start=0;
-	
-    for(int i=0;i<len;i++)
-    {
-        dp[i][i] = 1;
+
+    int expandAroundCenter(const string &s, int left, int right) {
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
     }
-    for(int i=0;i<len-1;i++)
-    {
-        if(s[i]==s[i+1])
-        { dp[i][i+1]=1;start=i;end=2;}
+
+
+    string longestPalindrome(string s) {
+        if (s.empty()) return "";
+        int start = 0, end = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            // Expand around the single character center (odd length palindromes)
+            int len1 = expandAroundCenter(s, i, i);
+            // Expand around the space between two characters center (even length palindromes)
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        
+        return s.substr(start, end - start + 1);
     }
     
-    for(int j=2;j<len;j++)
-    {
-        for(int i=0;i< len-j;i++)
-        {  
-            int left=i; //start point
-            int right = i+j;  //ending point
-            
-            if(dp[left+1][right-1]==1 && s[left]==s[right]) 
-            {
-                dp[left][right]=1; start=i; end=j+1; 
-            }        
-        }
-    }
-   return s.substr(start, end);
-}
+
+    
 };
