0af4058a9d17c583c227ee3c54548bd89ef1db82
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-06-05 06:11:27 +0530

    Time: 42 ms (60.01%) | Memory: 174 MB (18.07%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,46 +1,32 @@
 class Solution {
 public:
+    string expand(int i, int j, string s) {
+        int left = i;
+        int right = j;
 
-    int expandAroundCenter(const string &s, int left, int right) {
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {  
-            //expand till palindrome
-            left--;    
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            left--;
             right++;
         }
-        //length of palindromic string
-        return (right - 1) - (left + 1) + 1;
-    }
 
+        return s.substr(left + 1, right - left - 1);
+    }
 
     string longestPalindrome(string s) {
-        if (s.empty()) return "";
-        int start = 0, end = 0;
-        int len;
-        
-        for (int i = 0; i < s.size(); i++) 
-        {
-            // Expand around the single character center (odd length palindromes)
-            int len1 = expandAroundCenter(s, i, i);
-            // Expand around the space between two characters center (even length palindromes)
-            int len2 = expandAroundCenter(s, i, i + 1);
-            len = max(len1, len2);
-            //to store the start and end of pointers
-            if (len > end - start) //new highest maximum length
-            {
-                //explanation
-                // if i=2 len=4 ie. abbbb
-                //                    ^
-                //.  new end= i+(len/2)
-                //.  new start= i-(len-1)/2  
+        string ans = "";
 
-                start = i - (len - 1) / 2;    //we are going bacK.  new start<---start. (bigger len)
-                end = i + len / 2;          //we are going forward.  end---->new end (bigger len)
+        for (int i = 0; i < s.size(); i++) {
+            string odd = expand(i, i, s);
+            if (odd.size() > ans.size()) {
+                ans = odd;
+            }
+
+            string even = expand(i, i + 1, s);
+            if (even.size() > ans.size()) {
+                ans = even;
             }
         }
-        
-        return s.substr(start,end-start+1);
-    }
-    
 
-    
+        return ans;
+    }
 };
