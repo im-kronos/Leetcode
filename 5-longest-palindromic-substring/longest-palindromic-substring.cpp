1f8aa44b95d2722c9a3177d0d5e02daef3646093
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2024-06-05 06:04:25 +0530

    Time: 8 ms (91.90%) | Memory: 8.3 MB (74.81%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -2,31 +2,43 @@ class Solution {
 public:
 
     int expandAroundCenter(const string &s, int left, int right) {
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            left--;
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {  
+            //expand till palindrome
+            left--;    
             right++;
         }
-        return right - left - 1;
+        //length of palindromic string
+        return (right - 1) - (left + 1) + 1;
     }
 
 
     string longestPalindrome(string s) {
         if (s.empty()) return "";
         int start = 0, end = 0;
+        int len;
         
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.size(); i++) 
+        {
             // Expand around the single character center (odd length palindromes)
             int len1 = expandAroundCenter(s, i, i);
             // Expand around the space between two characters center (even length palindromes)
             int len2 = expandAroundCenter(s, i, i + 1);
-            int len = max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+            len = max(len1, len2);
+            //to store the start and end of pointers
+            if (len > end - start) //new highest maximum length
+            {
+                //explanation
+                // if i=2 len=4 ie. abbbb
+                //                    ^
+                //.  new end= i+(len/2)
+                //.  new start= i-(len-1)/2  
+
+                start = i - (len - 1) / 2;    //we are going bacK.  new start<---start. (bigger len)
+                end = i + len / 2;          //we are going forward.  end---->new end (bigger len)
             }
         }
         
-        return s.substr(start, end - start + 1);
+        return s.substr(start,end-start+1);
     }
     
 
