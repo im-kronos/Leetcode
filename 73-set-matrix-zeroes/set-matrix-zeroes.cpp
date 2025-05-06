f9966f55e123bf19467b3e0fc0e2775abe62593c
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-05-06 23:20:12 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (79.20%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,46 +1,35 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-
-        // Check which columns contain zeros
-        vector<bool> colZero(cols, false);
-        for (int j = 0; j < cols; ++j) {
-            for (int i = 0; i < rows; ++i) {
-                if (matrix[i][j] == 0) {
-                    colZero[j] = true;
-                    break;
+        vector<int>rows;
+        vector<int>columns;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    rows.push_back(i);
+                    columns.push_back(j);
                 }
             }
         }
 
-        // Check which rows contain zeros
-        vector<bool> rowZero(rows, false);
-        for (int i = 0; i < rows; ++i) {
-            for (int j = 0; j < cols; ++j) {
-                if (matrix[i][j] == 0) {
-                    rowZero[i] = true;
-                    break;
-                }
+        // Set entire rows to 0
+        for(int i = 0; i < rows.size(); i++)
+        {
+            for(int j = 0; j < matrix[0].size(); j++)
+            {
+                matrix[rows[i]][j] = 0;
             }
         }
 
-        // Set zeros for rows
-        for (int i = 0; i < rows; ++i) {
-            if (rowZero[i]) {
-                for (int j = 0; j < cols; ++j) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
-        // Set zeros for columns
-        for (int j = 0; j < cols; ++j) {
-            if (colZero[j]) {
-                for (int i = 0; i < rows; ++i) {
-                    matrix[i][j] = 0;
-                }
+        // Set entire columns to 0
+        for(int i = 0; i < matrix.size(); i++)
+        {
+            for(int j = 0; j < columns.size(); j++)
+            {
+                matrix[i][columns[j]] = 0;
             }
         }
     }
