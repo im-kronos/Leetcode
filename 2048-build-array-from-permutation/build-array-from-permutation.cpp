de1c4a51e096aac42da7364c8eb8b43906fcd012
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-05-06 21:02:00 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (58.11%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.cpp
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int>ans;
-        for(int i=0;i<nums.size();i++)
-        {
-            ans.push_back(nums[nums[i]]);
-        }
-        return ans;
+        for (int i = 0; i < nums.size(); i++) 
+            nums[i] += (1000 * (nums[nums[i]] % 1000));
+        
+        for (int i = 0; i < nums.size(); i++) 
+            nums[i] /= 1000;
+        
+        return nums;
     }
 };
