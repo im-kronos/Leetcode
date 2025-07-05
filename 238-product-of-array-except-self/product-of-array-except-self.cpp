62e5f1f35dcaa2adcceca434cd2acdb147be104b
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-07-05 19:55:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (16.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,56 +1,30 @@
 class Solution {
 public:
+    //(Using extra space)
+    // T.C : O(n)
+    // S.C : O(n)
+    // cal product of without itself for right and left and then take product
+    // for each index and add into res 1  1  2  6 24 12 4  1
     vector<int> productExceptSelf(vector<int>& nums) {
-        int countzero=0;
-        int productans=1;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]==0)
-            {
-                countzero++;
-            }
-            else
-            {
-                productans*=nums[i];
-            }
+        int n = nums.size();
+        vector<int> left(n);
+        vector<int> right(n);
+
+        // left side product
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
         }
-        if(countzero>1)
-        {
-            for(int i=0;i<nums.size();i++)
-            {
-                nums[i]=0;
-            }
+        // right side product
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
         }
-        else if(countzero==1)
-        {
-            for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]!=0)
-                {
-                nums[i]=0;
-                }
-                else
-                {
-                    nums[i]=productans;
-                }
-            }
-        }
-        else
-        {
-             for(int i=0;i<nums.size();i++)
-            {
-                if(nums[i]!=0)
-                {
-                nums[i]=productans/nums[i];
-                }
-                else
-                {
-                    nums[i]=productans;
-                }
-            }
+        vector<int> result(n);
+        for (int i = 0; i < n; i++) {
+            result[i] = left[i] * right[i];
         }
 
-        return nums;
-        }
-
+        return result;
+    }
 };
