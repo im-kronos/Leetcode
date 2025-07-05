19d9906817e25d2e14c84b6f0cc2abe08fba2cd2
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-07-05 20:41:02 +0530

    Time: 66 ms (75.71%) | Memory: 65.3 MB (75.77%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -8,14 +8,11 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.empty()) return 0;
-
-        // Min-heap
         priority_queue<int, vector<int>, greater<int>> pq;
         for (int i = 0; i < nums.size(); i++) {
             pq.push(nums[i]);
         }
-
-        int count = 1; // at least one element exists
+        int count = 1; 
         int temp = pq.top();
         pq.pop();
         int maxi = 1;
@@ -25,15 +22,12 @@ public:
             pq.pop();
 
             if (curr == temp) {
-                // Skip duplicates
                 continue;
             }
 
             if (curr == temp + 1) {
-                // Consecutive
                 count++;
             } else {
-                // Sequence breaks
                 count = 1;
             }
 
