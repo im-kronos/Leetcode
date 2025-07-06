cfcb419d3f32bafa0e6b6d4de200ca1a694e8ab6
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-07-06 19:46:16 +0530

    Time: 40 ms (48.44%) | Memory: 23.3 MB (49.88%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    
-    int changeCoinRec(int amount, vector<int>& coins,vector<int>&dp){
-        if(amount==0)
-            return 0;
-        
-        if(dp[amount]!=-1)
+    int helper(vector<int>& coins, int amount , vector<int>&dp) {
+        if (amount == 0) return 0;  
+        if (amount < 0) return INT_MAX;   
+        if(dp[amount]!= -1)
         {
             return dp[amount];
-        }
-        int ans=INT_MAX;
-        for(auto coin:coins){
-            if(amount-coin>=0){
-                int subProb=changeCoinRec(amount-coin, coins,dp);
-                if(subProb!=INT_MAX){
-                    ans=min(ans, subProb+1);
-                }
+        }  
+        int minCoins = INT_MAX;
+        for (int coin : coins) {
+            int res = helper(coins, amount - coin, dp);
+            if (res != INT_MAX) {
+                minCoins = dp[amount]=min(minCoins, 1 + res);
             }
         }
-        return dp[amount]=ans;
+
+        return dp[amount]=minCoins;
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans=changeCoinRec(amount, coins,dp);
-        return (ans==INT_MAX ? -1 : ans);
+        vector<int>dp(10001, -1);
+        int result = helper(coins, amount,dp);
+        return (result == INT_MAX) ? -1 : result;
     }
 };
