2703c3378e36e06696ba097f1c2b2354fbcbeb63
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-07-06 15:21:34 +0530

    Time: 21 ms (23.45%) | Memory: 14.3 MB (22.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,13 +3,16 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> window;
         int left = 0, right = 0, maxLen = 0;
-        
+
         while (right < s.length()) {
             if (!window.count(s[right])) {
                 window.insert(s[right]);
                 maxLen = max(maxLen, right - left + 1);
                 right++;
             } else {
+
+                //since it is alreay in a while loop this 
+                //will continue till the duplicate one is found and is remoeved
                 window.erase(s[left]);
                 left++;
             }
