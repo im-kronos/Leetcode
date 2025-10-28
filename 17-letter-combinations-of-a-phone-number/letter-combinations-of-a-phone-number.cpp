b04f64ed8049480a39822174aedc2c7fa41df934
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-10-29 01:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (9.97%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -4,12 +4,14 @@ public:
     vector<string> ans;
     
     void solve(string digits, int i, string word) {
-        //base case if whole string in digits.size() gets over
+        //base case if whole string in digits.size() ie the answer 
+        //whatever comes has reched the last index so it will be the answer
         if(i == digits.length()) {
             ans.push_back(word);
             return;
         }
-
+        
+        // this will take each char form particular index of digits[index] and add to word
         for(char ch : keypad[digits[i] - '0']) {
             solve(digits, i + 1, word + ch);
         }
