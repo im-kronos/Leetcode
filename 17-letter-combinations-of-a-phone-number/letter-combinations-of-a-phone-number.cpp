5d4f3a3e0760a5613f36b763dd4a6861a1307973
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2025-10-29 01:15:13 +0530

    Time: 2 ms (15.68%) | Memory: 9.5 MB (25.00%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    vector<string> keypad{" "," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    vector<string> keypad{""," ","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
     vector<string> ans;
-    vector<string> letterCombinations(string digits) {
-        for(auto t:keypad[digits[0]-'0']){
-            string s(1, t); 
-            ans.push_back(s);
+    
+    void solve(string digits, int i, string word) {
+        //base case if whole string in digits.size() gets over
+        if(i == digits.length()) {
+            ans.push_back(word);
+            return;
         }
 
-        for(int i=1;i<digits.size();i++) {
-            vector<string>temp;
-            for(auto t:keypad[digits[i]-'0'])
-            {
-                for(auto a:ans)
-                {
-                    temp.push_back(a+t);
-                }
-            }
-            ans.clear();
-            ans = temp;
+        for(char ch : keypad[digits[i] - '0']) {
+            solve(digits, i + 1, word + ch);
         }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        solve(digits, 0, "");
         return ans;
     }
 };
