14431292a230229100bfce81a70537ef4e71707b
Dharshan M <66472781+im-kronos@users.noreply.github.com>
2026-03-07 02:09:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (46.67%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,29 +1,9 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        if(s.size()==1 && s[0]=='1')
-        {
-            return true;
-        } else if(s.size()==1 && s[0]=='0')
-        {
-            return false;
-        }
-        
-        int flag =0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='1' && s[i+1]=='0' && flag == 0)
-            {
-                flag++;
-                continue;
-            }
-
-            if(flag != 0 && s[i]=='1')
-            {
-                return false;
-            }
+        for(int i=0;i<s.size()-1;i++){
+            if(s[i] == '0' && s[i+1] == '1') return false;
         }
         return true;
-        
     }
 };
